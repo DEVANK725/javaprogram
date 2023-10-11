82746ecd962c23342c735e0e28a2e3ced1851d13
DEVANK725 <144879987+DEVANK725@users.noreply.github.com>
2023-10-11 11:49:15 +0530

    Add files via upload

---
+++ Array6.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class Array6
+{
+	public static void main(String[] args) {
+	    int[]a={1,2,3,4,5};
+	    int sum=0;
+	    for(int i=0;i<a.length;i++)
+	    {
+	        sum=sum+a[i];
+	    }
+	    System.out.println("sum = "+sum);
+	    System.out.println("avg = "+sum/a.length);
+	}
+}
