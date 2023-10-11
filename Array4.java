ccc8ee371bfa56e2bb9de4e0d0569ce78433d0bf
DEVANK725 <144879987+DEVANK725@users.noreply.github.com>
2023-10-11 11:44:30 +0530

    Add files via upload

---
+++ Array4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Array4
+{
+	public static void main(String[] args) {
+	    int[][] board=new int[3][3];
+	    for(int i=0;i<board.length;i++)
+	    {
+	        for(int j=0;j<board[i].length;j++)
+	        {
+	            board[i][j]=i+j;
+	        }
+	    }
+	    for(int i=0;i<board.length;i++)
+	    {
+	        for(int j=0;j<board[i].length;j++)
+	        {
+	            System.out.print(board[i][j]+" ");
+	        }
+	        System.out.println();
+	    }
+	}
+}
