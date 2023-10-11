4b5c1ccfe1c2d25025d31fb01bd2d9d30d268f35
DEVANK725 <144879987+DEVANK725@users.noreply.github.com>
2023-10-11 12:18:15 +0530

    Add files via upload

---
+++ FindString.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class FindString
+{
+	public static void main(String[] args) {
+	    String a="Umbrella";
+	    boolean per=false;
+	    for(int i=0;i<a.length();i++)
+	    {
+		if(a.charAt(i)=='e')
+		{
+	        per=true;
+	        break;
+		}
+	    }
+	    System.out.println(per);
+	}
+}
