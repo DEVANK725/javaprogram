4d4277c10ef0e2c2d5a609289799f1d896d1e6f2
DEVANK725 <144879987+DEVANK725@users.noreply.github.com>
2023-10-11 12:31:57 +0530

    Add files via upload

---
+++ NonSquareMatrix.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class NonSquareMatrix
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String [][]a={{"Mr.","Mrs.","Ms."},{"Kumar"}};
+	        System.out.println(a[0][0] + a[1][0]);
+	        System.out.println(a[0][1] + a[1][0]);
+	        System.out.println(a[0][2] + a[1][0]);
+	}
+}
