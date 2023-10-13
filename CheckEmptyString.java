5308103ed0e077aece0871057b243c35c5c6056c
DEVANK725 <144879987+DEVANK725@users.noreply.github.com>
2023-10-13 16:20:49 +0530

    Add files via upload

---
+++ CheckEmptyString.java
@@ -0,0 +1,13 @@
+public class CheckEmptyString
+{
+	public static void main(String[] args) {
+	    //create a string
+	    String greet  = "Hello! World";
+		System.out.println("String : "+ greet);
+		
+	  // Check empty String
+	  boolean b = greet.isEmpty();
+		System.out.println(b);
+		System.out.println(greet.isEmpty());
+	}
+}
