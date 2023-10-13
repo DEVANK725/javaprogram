f04ed08862d3909830aba398744589df67c3bb3e
DEVANK725 <144879987+DEVANK725@users.noreply.github.com>
2023-10-13 16:22:34 +0530

    Add files via upload

---
+++ ClassandObject.java
@@ -0,0 +1,8 @@
+public class ClassandObject {
+    int x=5;
+    public static void main(String[] args) {
+        ClassandObject obj=new ClassandObject();
+        System.out.print(obj.x);
+    }
+
+}
