9c7d6048b7f33bac5dbe66d25997232605116997
DEVANK725 <144879987+DEVANK725@users.noreply.github.com>
2023-12-20 12:15:16 +0530

    Add files via upload

---
+++ ThreadCount.java
@@ -0,0 +1,11 @@
+public class ThreadEx1 extends Thread
+{
+	public static void main(String[] args) {
+	    ThreadEx1 Thread=new ThreadEx1();
+		System.out.println("This code is outside of the thread");
+	}
+	public void run()
+	{
+	   System.out.println("This code is runnig in the thread"); 
+	}
+}
